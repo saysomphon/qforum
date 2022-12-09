0d385b984d454d14671e2ec611bfa19740e96cae
Khamphouvong.kcsv@gmail.com <Khamphouvong.kcsv@gmail.com>
2022-12-10 04:07:32 +0700

    Revert "add route"
    This reverts commit eb0f1fc25c4fff11430482e51fd50b764ad8c489.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:no_context_navigation/no_context_navigation.dart';
 import 'package:qfoumn/firebase_options.dart';
-import 'package:qfoumn/features/sign_in/presentation/screen/sign_in_screen.dart';
+import 'package:qfoumn/google_screen.dart';
 import 'package:qfoumn/theme.dart';
-
-import 'core/routes/router.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,26 +12,63 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      //disable debug
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: theme(),
-      home: const SignInScreen(),
-      //set up navigation
-      navigatorKey: NavigationService.navigationKey,
-      onGenerateRoute: (settings) => AppRoute.routeGenerate(settings),
-      initialRoute: AppRoute.initialRoute,
+      home: const GoogleSigninScreen(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
