0a44a63a61950dd50c31b95caefbdec5ced7f151
ting.iquritech <ting.iquritech@gmail.com>
2022-12-10 13:10:43 +0700

    fix conflict with

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,5 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:qfoumn/constants/colors.dart';
 import 'package:qfoumn/constants/padding.dart';
+import 'package:qfoumn/screens/home/widgets/card_forum_type.dart';
 import 'package:qfoumn/screens/home/widgets/notification_icon.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -10,14 +13,67 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final Stream<QuerySnapshot> _forumTypeStream =
+      FirebaseFirestore.instance.collection('forum_type').snapshots();
+  int _selectedIndex = 0;
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         actions: [NotificationIconButton()],
       ),
-      body: Padding(
-          padding: const EdgeInsets.all(PaddingConstant.scaffoldPadding)),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: _forumTypeStream,
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return const Text('Something went wrong');
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(
+                color: Colors.white,
+              ),
+            );
+          }
+
+          return Padding(
+            padding: const EdgeInsets.all(PaddingConstant.scaffoldPadding),
+            child: ListView(
+              children: snapshot.data!.docs.map((DocumentSnapshot document) {
+                Map<String, dynamic> data =
+                    document.data()! as Map<String, dynamic>;
+                return Padding(
+                  padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+                  child: CardForumType(title: data['name']),
+                );
+              }).toList(),
+            ),
+          );
+        },
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Setting',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        backgroundColor: ColorsConstant.primaryColor,
+        selectedItemColor: Colors.white,
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
