4a22500ad41d69933f26fdbb699ca9f4408c2c75
Bank <Keonakhone1998@gmail.com>
2022-12-10 10:23:15 +0700

    setting ui design

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:qfoumn/firebase_options.dart';
 import 'package:qfoumn/screens/home/home_screen.dart';
 import 'package:qfoumn/screens/post/post_screen.dart';
+import 'package:qfoumn/screens/setting/setting_screen.dart';
 import 'package:qfoumn/theme.dart';
 
 void main() async {
@@ -23,54 +24,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: theme(),
-      home: PostScreen(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      home: SettingScreen(),
     );
   }
 }
---
+++ lib/screens/setting/setting_screen.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+class SettingScreen extends StatefulWidget {
+  const SettingScreen({super.key});
+
+  @override
+  State<SettingScreen> createState() => _SettingScreenState();
+}
+
+class _SettingScreenState extends State<SettingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Icon(Icons.arrow_back_ios),
+        centerTitle: true,
+        title: const Text(
+          'Setting',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+      body: Container(
+        color: const Color.fromARGB(255, 25, 97, 156),
+        child: ListView(
+          padding: const EdgeInsets.all(8),
+          children: [
+            SettingList(
+              icon: Icons.notifications,
+              title: 'Notification',
+              onTap: () {},
+            ),
+            SettingList(
+              icon: Icons.account_balance_outlined,
+              title: 'About Us',
+              onTap: () {},
+            ),
+            SettingList(
+              icon: Icons.logout,
+              title: 'Logout',
+              onTap: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SettingList extends StatelessWidget {
+  final IconData icon;
+  final String title;
+  final GestureTapCallback onTap;
+
+  const SettingList(
+      {Key? key, required this.icon, required this.title, required this.onTap})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        decoration: const BoxDecoration(
+          border: Border(
+            bottom: BorderSide(width: 2.0, color: Colors.grey),
+          ),
+        ),
+        height: 60,
+        child: Row(
+          children: [
+            Icon(
+              icon,
+              size: 30,
+              color: Colors.white,
+            ),
+            const SizedBox(width: 5),
+            Text(
+              title,
+              style: const TextStyle(color: Colors.white),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
